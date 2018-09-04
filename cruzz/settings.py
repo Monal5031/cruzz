4741fd8102f0f3029d68094f4fd3d26fbeb55f84
Monal Shadi <monalshadi16@gmail.com>
2018-09-04 18:58:12 +0530

    Model and migrations for custom user. (#2)
    * Model CustomUser
      -- Migrations

--- cruzz/settings.py
+++ cruzz/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    'landing',
     'pwa',
     'widget_tweaks',
+    'landing.apps.LandingConfig'
 ]
 
 MIDDLEWARE = [
--- landing/models.py
+++ landing/models.py
@@ -1,4 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+
+
+class CustomUser(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=20, null=True, blank=True)
+    last_name = models.CharField(max_length=20, null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    state = models.CharField(max_length=50, null=True, blank=True)
+    country = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(max_length=50, null=True, blank=True, unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.user.username)
