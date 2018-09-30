8e3c29f45e95742d95db94189241b02da380be3c
Monal Shadi <monalshadi16@gmail.com>
2018-09-30 23:12:43 +0530

    initial login, tests are left (#7)
    * initial login, tests are left
    
    * Add admin.py
    
    * Fix logout URL redirect

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 Welcome aboard!
 
 [![Build Status](https://travis-ci.com/mohitkyadav/cruzz.svg?token=pLJkWav1wbnbGfoLfZtG&branch=master)](https://travis-ci.com/mohitkyadav/cruzz)
+
+
+Steps to setup:
+
+```bash
+    virtualenv -p python3 venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+```
+
+
+
+```
+Credentials:
+    username: admin
+    password: admin12345
+```
--- landing/views.py
+++ landing/views.py
@@ -1,10 +1,15 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeForm
-from django.contrib.auth import update_session_auth_hash
-from django.contrib import messages
-from django.shortcuts import render, redirect
+# Django
+from django.shortcuts import render
 from django.views import View
-from social_django.models import UserSocialAuth
+from rest_framework import status
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+# Local Django
+from landing.serializers import RegistrationSerializer, LoginSerializer, UserSerializer
+from landing.renderers import UserJSONRenderer
 
 
 def land(request):
@@ -19,3 +24,67 @@ class HomeView(View):
 # only4 testing <3
 def test_func():
     return True
+
+
+class RegistrationAPIView(APIView):
+    # Allow any user (authenticated or not) to hit this endpoint.
+    permission_classes = (AllowAny,)
+    renderer_classes = (UserJSONRenderer,)
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+        user = request.data.get('user', {})
+
+        # The create serializer, validate serializer, save serializer pattern
+        # below is common.
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    renderer_classes = (UserJSONRenderer,)
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        user = request.data.get('user', {})
+
+        # Notice here that we do not call `serializer.save()` like we did for
+        # the registration endpoint. This is because we don't  have
+        # anything to save. Instead, the `validate` method on our serializer
+        # handles everything we need.
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (UserJSONRenderer,)
+    serializer_class = UserSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        # There is nothing to validate or save here. Instead, we just want the
+        # serializer to handle turning our `User` object into something that
+        # can be JSONified and sent to the client.
+        serializer = self.serializer_class(request.user)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        serializer_data = request.data.get('user', {})
+
+        # Here is that serialize, validate, save pattern we talked about
+        # before.
+        serializer = self.serializer_class(
+            request.user, data=serializer_data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
