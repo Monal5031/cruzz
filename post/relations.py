f787ade202b3fc68b6d49ca5fc790b2351e6800f
Monal5031 <monalshadi16@gmail.com>
2018-10-29 00:04:45 +0530

    Complete relations

---
+++ post/relations.py
@@ -0,0 +1,17 @@
+# Django
+from rest_framework import serializers
+
+# local django
+from post.models import Tag
+
+
+class TagRelatedField(serializers.RelatedField):
+    def get_queryset(self):
+        return Tag.objects.all()
+
+    def to_internal_value(self, data):
+        tag, created = Tag.objects.get_or_create(tag=data, slug=data.lower())
+        return tag
+
+    def to_representation(self, value):
+        return value.tag
