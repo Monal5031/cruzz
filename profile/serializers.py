8ef111021bc43f7df93accd83421d007e49359c5
Monal5031 <monalshadi16@gmail.com>
2018-10-29 00:05:20 +0530

    serialize api

--- profile/serializers.py
+++ profile/serializers.py
@@ -9,10 +9,11 @@ class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username')
     bio = serializers.CharField(allow_blank=True, required=False)
     image = serializers.SerializerMethodField()
+    following = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
-        fields = ('username', 'bio', 'image')
+        fields = ('username', 'bio', 'image', 'following',)
         read_only_fields = ('username',)
 
     def get_image(self, obj):
@@ -20,3 +21,19 @@ class ProfileSerializer(serializers.ModelSerializer):
             return obj.image
 
         return 'https://thumb.ibb.co/eN5O0f/temp.jpg'
+
+    def get_following(self, instance):
+        request = self.context.get('request', None)
+
+        if request is None:
+            return False
+
+        if not request.user.is_authenticated:
+            return False
+
+        follower = request.user.profile
+        followee = instance
+        print(followee)
+        print(follower)
+
+        return follower.is_following(followee)
