90dd275c6897bab0b1fd0cac65fa6790c9edab37
Monal5031 <monalshadi16@gmail.com>
2018-11-06 19:58:55 +0530

    Cleanup :clamp:

--- profile/serializers.py
+++ profile/serializers.py
@@ -11,11 +11,10 @@ class ProfileSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
     following = serializers.SerializerMethodField()
     cover = serializers.SerializerMethodField()
-    user = serializers.SerializerMethodField(required=False)
 
     class Meta:
         model = Profile
-        fields = ('username', 'bio', 'image', 'following', 'cover', 'user')
+        fields = ('username', 'bio', 'image', 'following', 'cover')
         read_only_fields = ('username',)
 
     def get_image(self, obj):
@@ -43,6 +42,3 @@ class ProfileSerializer(serializers.ModelSerializer):
         followee = instance
 
         return follower.is_following(followee)
-
-    def get_user(self, obj):
-        print(obj)
